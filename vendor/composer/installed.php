e792b6a4bd25f68932cbbbabf0c48878823df1c4
Julien <j.halfkan@webdigit.be>
2024-03-28 16:19:36 +0100

    add vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,42 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '9d7e36d3a110326f0fb1e6bc14a303475feaaf28',
+    'name' => 'julienhalfkan/webdigit-library',
+  ),
+  'versions' => 
+  array (
+    'julienhalfkan/webdigit-library' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9d7e36d3a110326f0fb1e6bc14a303475feaaf28',
+    ),
+    'smalot/pdfparser' => 
+    array (
+      'pretty_version' => 'v2.2.1',
+      'version' => '2.2.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '7c007090776f29119e72be28f7780e638ea0cbf4',
+    ),
+    'symfony/polyfill-mbstring' => 
+    array (
+      'pretty_version' => 'v1.26.0',
+      'version' => '1.26.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9344f9cb97f3b19424af1a21a3b0e75b0a7d8d7e',
+    ),
+  ),
+);
